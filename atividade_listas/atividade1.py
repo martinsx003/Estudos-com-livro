51b7dfc100d84ef51bec92d9dda8abbfc98d4a20
martinsx003 <pf847725@gmail.com>
2025-01-05 18:02:28 -0300

    dando commit

---
+++ atividade_listas/atividade1.py
@@ -0,0 +1,6 @@
+name = ['laura', 'miguel', 'stefani', 'joao']
+mens = "o nome é: "
+print (f"{mens}{name[0]}\n")
+print (f"{mens}{name[1]}\n")
+print (f"{mens}{name[2]}\n")
+print (f"{mens}{name[3]}")
